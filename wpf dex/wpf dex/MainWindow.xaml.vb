0493e85241f3d1ac89963076fadf2738dcaa568e
bjhal <bjhal401@learner.dvusd.org>
2024-12-03 12:57:01 -0700

    colors

--- wpf dex/wpf dex/MainWindow.xaml.vb
+++ wpf dex/wpf dex/MainWindow.xaml.vb
@@ -4,6 +4,7 @@
         bul.Visibility = Visibility.Hidden
         charm.Visibility = Visibility.Hidden
         natxt.Text = pibu.Content
+        cotxt.Text = "yellow"
     End Sub
 
     Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
@@ -11,6 +12,7 @@
         bul.Visibility = Visibility.Hidden
         charm.Visibility = Visibility.Visible
         natxt.Text = chbu.Content
+        cotxt.Text = "orange"
     End Sub
 
     Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
@@ -18,5 +20,6 @@
         bul.Visibility = Visibility.Visible
         charm.Visibility = Visibility.Hidden
         natxt.Text = bubu.Content
+        cotxt.Text = "green"
     End Sub
 End Class
